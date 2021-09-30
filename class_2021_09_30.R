18c25b5c23a271b9195cb4fb6a07f2ca10e18544
Colin Rundel <rundel@gmail.com>
2021-09-30 13:14:07 -0400

    In clas

--- class_2021_09_30.R
+++ class_2021_09_30.R
@@ -36,3 +36,48 @@ movies = tibble::tibble(
 )
 
 movies
+
+## Exercise 1
+
+
+
+scrape_movie_page = function(url) {
+  
+  page = read_html(url)
+  
+  list(
+    n_review = page %>% 
+      html_elements(".scoreboard__link--tomatometer") %>%
+      html_text() %>%
+      str_remove(" Reviews") %>%
+      as.integer(),
+ 
+    n_rating = page %>%
+      html_elements(".scoreboard__link--audience") %>%
+      html_text() %>%
+      str_remove(" Ratings"),
+    
+    runtime = page %>%
+      html_elements(".scoreboard__info") %>%
+      html_text() %>%
+      str_split(", ", simplify = TRUE) %>%
+      {.[,3]},
+    
+    aud_score = page %>%
+      html_elements(".scoreboard") %>% 
+      html_attr("audiencescore") %>%
+      as.numeric() %>%
+      {. / 100}
+    
+  )
+}
+
+movies %>%
+  mutate(
+    details = purrr::map(url, scrape_movie_page)
+  ) %>%
+  unnest_wider(details) %>% 
+  View()
+
+
+
