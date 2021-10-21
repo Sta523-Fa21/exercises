1fb1e935a9e0f5bc07bcafb108156b9661a55720
Colin Rundel <rundel@gmail.com>
2021-10-21 13:17:00 -0400

    class

--- class_2021_10_21.R
+++ class_2021_10_21.R
@@ -21,4 +21,28 @@ dplyr::copy_to(
 
 DBI::dbDisconnect(db)
 
-#
+
+## Exercise 1
+
+db = DBI::dbConnect(
+  RSQLite::SQLite(), 
+  here::here("class_2021_10_21/flights.sqlite")
+)
+
+data(flights, package = "nycflights13")
+data(planes, package = "nycflights13")
+
+tidyquery::query(
+  "select sum(seats) from flights natural left join planes"
+)
+
+tidyquery::show_dplyr(
+  "select sum(seats) from flights natural left join planes"
+)
+
+tidyquery::query(
+  "select sum(seats) from flights left join planes using (tailnum)" 
+)
+
+
+
